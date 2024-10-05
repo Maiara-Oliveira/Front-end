c1f5400444e363b6427e135ab37c5c4a1b087dcb
Maiara Oliveira <maiara.felipe4@gmail.com>
2024-10-04 21:43:13 -0300

    Add files via upload

---
+++ RadioButtonTest.java
@@ -0,0 +1,13 @@
+
+import javax.swing.JFrame; // importamdo biblioteca
+
+public class RadioButtonTest  // declaração de exportação/exportando uma classe
+{
+   public static void main(String[] args) // 
+   {
+      RadioButtonFrame radioButtonFrame = new RadioButtonFrame();
+      radioButtonFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      radioButtonFrame.setSize(300, 100); 
+      radioButtonFrame.setVisible(true); 
+   } 
+} 
