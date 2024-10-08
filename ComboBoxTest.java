07dd4e1f17c27a8ebfe1397bc23fbbeb1dc1d69d
Maiara Oliveira <maiara.felipe4@gmail.com>
2024-10-07 21:47:38 -0300

    Add files via upload

---
+++ ComboBoxTest.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class ComboBoxTest
+{ //  começar o bloco de codigos da classe
+   public static void main(String[] args)
+   { 
+      ComboBoxFrame comboBoxFrame = new ComboBoxFrame(); 
+      comboBoxFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      comboBoxFrame.setSize(350, 150); 
+      comboBoxFrame.setVisible(true); 
+   } 
+} 
